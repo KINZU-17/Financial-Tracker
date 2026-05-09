71ff7fa44b0523e03d743ffa998691d97714526c
James Nzuki <nzukijames200@gmail.com>
2026-05-09 13:23:44 +0300

    Start up progress

---
+++ src/data/content.js
@@ -0,0 +1,4 @@
+export const expenses = [
+  { id: 1, date: "2026-05-01", category: "Food", description: "Lunch", amount: 15.00 },
+  { id: 2, date: "2026-05-02", category: "Transport", description: "Uber", amount: 25.50 },
+];
